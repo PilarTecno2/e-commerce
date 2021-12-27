3504cb8e03d2caf3cac1db9bd4c687499b4d7295
Matias N Zamora <nikoozamora93@gmail.com>
2021-12-27 11:43:21 -0300

    Actividad clase 2

--- src/components/HeaderApp.js
+++ src/components/HeaderApp.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { Col, Row } from 'react-bootstrap';
+import { Col, Container, Navbar, Row } from 'react-bootstrap';
+import { ShoppingCart } from "./ShoppingCart/ShoppingCart";
 
 
 // esto es un return inplisito solo se aplica cuando no tiene funcionalidades de js sino mas bien que es algo visual.
 // esto se exporto nombrado para poder poner headerapp en el app.js.
 export const HeaderApp = () => (
-    
-    <Row>
-        <Col xs = {12}>
-            <div> Mini Ecommerce </div>
-        </Col>
-    </Row>
-
+    <Navbar>
+        <Container>
+            <Row>
+                <Col xs = {12}>
+                    <div> Mini Ecommerce </div>
+                </Col>
+            </Row>
+            <ShoppingCart/>
+        </Container>
+    </Navbar>
 );
 
 
---
+++ src/components/ShoppingCart/ShoppingCartItem.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {NavDropdown} from 'react-bootstrap';
+
+
+export const ShoppingCartItem = () => (
+    <NavDropdown.Item href="#action/3.1">Actions</NavDropdown.Item>
+);
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
