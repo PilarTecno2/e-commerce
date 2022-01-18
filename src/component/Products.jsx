6aeac92d2844891b6c8c1f26c9654af4b75607fa
Matias N Zamora <nikoozamora93@gmail.com>
2022-01-18 03:12:36 -0300

    adding products section

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Skeleton from 'react-loading-skeleton';
+import { NavLink } from 'react-router-dom';
 
 export const Products = () => {
 
@@ -33,17 +34,17 @@ export const Products = () => {
         return (
             <>
                 <div className="col-md-3">
-                    <Skeleton height={350}/>
+                    <Skeleton height={350}  />
                 </div>
                 <div className="col-md-3">
-                    <Skeleton height={350}/>
+                    <Skeleton height={350} />
                 </div>
                 <div className="col-md-3">
-                    <Skeleton height={350}/>
+                    <Skeleton height={350} />
                 </div>
             </>
-        )
-    }
+        );
+    };
 
     const filterProduct = (cat) => {
         const updatedList = data.filter((x)=> x.category === cat);
@@ -71,9 +72,9 @@ export const Products = () => {
                                         <div className="card-body">
                                             <h5 className="card-title mb-0">{product.title.substring(0,12)}...</h5>
                                             <p className="card-text lead fw-bold">$ {product.price} </p>
-                                            <a href="#" className="btn btn-outline-dark">
+                                            <NavLink to = {`/products/${product.id}`} className="btn btn-outline-dark">
                                                 Comprar Ahora
-                                            </a>
+                                            </NavLink>
                                         </div>
                                 </div>
                             </div>
